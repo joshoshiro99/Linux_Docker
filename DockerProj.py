c73c187eeacb80a6e39e65fa0b5a32943e4ad1ef
Joshua Oshiro <oshi4230@fredonia.edu>
2020-04-14 17:08:51 -0400

    Added comments inline

--- DockerProj.py
+++ DockerProj.py
@@ -1,10 +1,17 @@
 import docker
 
+#create a docker client from the docker application running in the background.
 client = docker.from_env()
+
+#create a detached docker container with terminal functionality
 container = client.containers.run("ubuntu",stdin_open=True,tty=True, detach = True)
+
+#debugging docker status remaining as "created" only
 status = container.status
 container.start()
 status = container.status
+
+#command loop - take user command as cmd, run cmd in container, return logs, repeat until "exit"
 cmd = ""
 while cmd != "exit":
     cmd = input("The following commands are available for you to use: echo, ls , cd, touch, and ping\ntype \"exit\" to quit:\n")
