e9a96637449200d8954c90f9ed64c4f67a6e2987
Joshua Oshiro <oshi4230@fredonia.edu>
2020-04-15 18:42:46 -0400

    Fixed some commands not working. Currently prepends '/bin/sh -c'  to user input.  This allows most navigation to be enabled.

--- DockerProj.py
+++ DockerProj.py
@@ -6,7 +6,7 @@ client = docker.from_env()
 #create a detached docker container with terminal functionality
 container = client.containers.run("ubuntu",stdin_open=True,tty=True, detach = True)
 
-#debugging docker status remaining as "created" only
+#change container status from "created" to "running"
 status = container.status
 container.start()
 container.reload()
@@ -15,9 +15,10 @@ status = container.status
 #command loop - take user command as cmd, run cmd in container, return logs, repeat until "exit"
 cmd = ""
 while cmd != "exit":
-    cmd = input("The following commands are available for you to use: echo, ls , cd, touch, and ping\ntype \"exit\" to quit:\n")
+    cmd = input("The following commands are available for you to use: echo, ls, touch, mkdir and rmdir\ntype \"exit\" to quit:\n")
     if cmd == "exit":
         break
     else:
-        tuple = container.exec_run(cmd, tty=True,stdin=True,stdout=True)
+        command=["/bin/sh", "-c", cmd]
+        tuple = container.exec_run(command, tty=True,stdin=True,stdout=True)
         print(tuple[1])
